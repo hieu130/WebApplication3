6f6745f0a4a6527888e66d8c007e5ae073145d81
hieu130 <hieuvanphan275@gmail.com>
2022-04-29 13:10:08 +0700

    Add project files.

---
+++ WebApplication3/Controllers/AccountsController.cs
@@ -0,0 +1,170 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private WebApplication3Context db = new WebApplication3Context();
+        [HttpGet]
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+
+            ViewBag.ListAccount = this.db.Accounts.ToList();
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.IdSortParm = sortOrder == "Id" ? "id_desc" : "Id";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var accounts = from p in db.Accounts
+                           select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                accounts = accounts.Where(p => p.UserName.Contains(searchString) || p.Address.Contains(searchString) || p.RoleNumber.Contains(searchString) || p.EmployeeNumber.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+
+                case "id_desc":
+                    accounts = accounts.OrderByDescending(p => p.Id);
+                    break;
+                default:
+                    accounts = accounts.OrderBy(p => p.Id);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(accounts.ToPagedList(pageNumber, pageSize));
+        }
+
+
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View(db.Accounts.ToList());
+        }
+
+        // GET: Accounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Password,DisabledAt,Address,Thumbnail,Status,RoleNumber,EmployeeNumber,AddmissionDate,DateOfJoining")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Password,DisabledAt,Address,Thumbnail,Status,RoleNumber,EmployeeNumber,AddmissionDate,DateOfJoining")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication3/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+        public DateTime DisabledAt { get; set; }
+        public string Address { get; set; }
+        public string Thumbnail { get; set; }
+        public int Status { get; set; }
+        public string RoleNumber { get; set; }
+        public string EmployeeNumber { get; set; }
+        public DateTime AddmissionDate { get; set; }
+        public DateTime DateOfJoining { get; set; }
+    }
+}
